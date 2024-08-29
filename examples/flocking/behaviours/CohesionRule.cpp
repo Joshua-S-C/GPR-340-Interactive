e3e2757e68b9a20ea2a3c33c71bc765ab4015af5
Joshua Sinclair Chong <joshua.sinclairchong@mymail.champlain.edu>
2024-08-29 10:54:59 -0400

    + Cohesion Rule

--- examples/flocking/behaviours/CohesionRule.cpp
+++ examples/flocking/behaviours/CohesionRule.cpp
@@ -7,7 +7,18 @@ Vector2f CohesionRule::computeForce(const std::vector<Boid*>& neighborhood, Boid
   // todo: add your code here to make a force towards the center of mass
   // hint: iterate over the neighborhood
 
+  if (neighborhood.size() == 0)
+    return cohesionForce;
+
+  Vector2f centreOfMass = Vector2f::zero();
+
   // find center of mass
+  for (Boid *neighbor : neighborhood)
+    centreOfMass += neighbor->getPosition();
+
+  centreOfMass /= neighborhood.size();
+
+  cohesionForce = (centreOfMass - boid->getPosition()) / boid->getDetectionRadius();
 
   return cohesionForce;
 }
