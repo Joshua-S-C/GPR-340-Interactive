0695c2596130ce13f8ae4ce32cfc3b1844cb973b
Joshua Sinclair Chong <joshua.sinclairchong@mymail.champlain.edu>
2024-08-29 11:22:32 -0400

    bluh

--- examples/flocking/behaviours/CohesionRule.cpp
+++ examples/flocking/behaviours/CohesionRule.cpp
@@ -2,18 +2,18 @@
 #include "../gameobjects/Boid.h"
 
 Vector2f CohesionRule::computeForce(const std::vector<Boid*>& neighborhood, Boid* boid) {
-  Vector2f cohesionForce;
+  Vector2f cohesionForce = Vector2f::zero();
 
   // todo: add your code here to make a force towards the center of mass
   // hint: iterate over the neighborhood
 
-  if (neighborhood.size() == 0)
+  if (neighborhood.empty())
     return cohesionForce;
 
   Vector2f centreOfMass = Vector2f::zero();
 
   // find center of mass
-  for (Boid *neighbor : neighborhood)
+  for (Boid* neighbor : neighborhood)
     centreOfMass += neighbor->getPosition();
 
   centreOfMass /= neighborhood.size();
