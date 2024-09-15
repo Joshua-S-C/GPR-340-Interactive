4fe93385f59a32ed9490e32444b927510f746dcd
Joshua Sinclair Chong <joshua.sinclairchong@mymail.champlain.edu>
2024-09-15 17:41:31 -0400

    + Working on hexagonal

--- examples/life/rules/HexagonGameOfLife.cpp
+++ examples/life/rules/HexagonGameOfLife.cpp
@@ -3,5 +3,73 @@
 //
 
 #include "HexagonGameOfLife.h"
-void HexagonGameOfLife::Step(World& world) {}
-int HexagonGameOfLife::CountNeighbors(World& world, Point2D point) { return 0; }
+void HexagonGameOfLife::Step(World& world) {
+    Point2D point = {0, 0};
+
+    for (int row = 0; row < world.SideSize(); row++, point.y = row) {
+        for (int col = 0; col < world.SideSize(); col++) {
+            point.x = col;
+
+            int neighbors = CountNeighbors(world, point);
+            bool alive = world.Get(point);
+
+            //Dead Cell
+            if (!alive) {
+                if (neighbors == 2)
+                    world.SetNext(point, true);
+                else
+                    world.SetNext(point, false);
+            } else {
+                // Live Cell
+                if (neighbors == 2)
+                    world.SetNext(point, true);
+                else
+                    world.SetNext(point, false);
+            }
+
+        }
+    }
+
+    world.SwapBuffers();
+}
+
+
+int HexagonGameOfLife::CountNeighbors(World& world, Point2D point) { 
+	int neighbors = 0;
+
+    if (point.x % 2 == 0) {
+        // Up Left
+        neighbors += world.Get(point + point.UP + point.LEFT);
+
+        // Up Right
+        neighbors += world.Get(point + point.UP);
+
+        // Down Left
+        neighbors += world.Get(point + point.DOWN + point.LEFT);
+
+        // Down Right
+        neighbors += world.Get(point + point.DOWN);
+    }
+
+    if (point.x % 2 == 1) {
+	    // Up Left
+	    neighbors += world.Get(point + point.UP);
+
+	    // Up Right
+	    neighbors += world.Get(point + point.UP + point.RIGHT);
+
+	    // Down Left
+	    neighbors += world.Get(point + point.DOWN);
+
+	    // Down Right
+        neighbors += world.Get(point + point.DOWN + point.RIGHT);
+    }
+
+    // Right
+    neighbors += world.Get(point + point.RIGHT);
+
+    // Left
+    neighbors += world.Get(point + point.LEFT);
+
+	return neighbors;
+}
