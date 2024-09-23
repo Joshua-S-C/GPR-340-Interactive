695e07b7477717d69ad19269133fa17a334370f1
Joshua Sinclair Chong <joshua.sinclairchong@mymail.champlain.edu>
2024-09-23 11:35:57 -0400

    + Starting DFS ? How do I find out which directions exist for neighbors

--- examples/maze/generators/RecursiveBacktrackerExample.cpp
+++ examples/maze/generators/RecursiveBacktrackerExample.cpp
@@ -4,7 +4,50 @@
 #include <climits>
 bool RecursiveBacktrackerExample::Step(World* w) {
   // todo: implement this
-  return false;
+
+    Point2D p;
+
+    // Default
+    if (stack.empty()) {
+        //p = randomStartPoint(w);
+        p = {0, 0};
+        stack.push_back(p);
+    } else {
+        p = stack.back();
+    }
+
+    // Check neighbors to visit
+    std::vector<Point2D> visitables = getVisitables(w, p);
+
+    // Check valid neighbor not in stack
+    if (!stack.empty() &&
+        !visitables.empty()
+        // Check that it's not off the world border
+        ) {
+
+        // Randomly select neighbor
+        int rando = randNums[randNumIndex];
+        randNumIndex++;
+        rando %= visitables.size();
+        Point2D neighbor = visitables[rando];
+
+        // Add to stack
+        stack.push_back(neighbor);
+
+        // Break wall between current and chosen neighbor // Using the SetNorth/South/Etc
+        Node neighborNode = w->GetNode(neighbor);
+
+        // Find out which direction it is!!!
+        w->SetNode(neighbor, );
+
+
+        return true;
+    } else {
+        stack.pop_back();
+    
+        return false;
+    }
+
 }
 
 void RecursiveBacktrackerExample::Clear(World* world) {
@@ -34,6 +77,24 @@ std::vector<Point2D> RecursiveBacktrackerExample::getVisitables(World* w, const 
   std::vector<Point2D> visitables;
 
   // todo: implement this
+
+  // World only stores walls. 
+  // GetNorth/South/etc gets the walls
+  // If you want to check if it is visited, get the color
+  // Up, Right, Down, Left
+  /*Gray*/
+
+  if (w->GetNodeColor(p + p.UP) == Color::DarkGray) 
+      visitables.push_back(p + p.UP);
+
+  if (w->GetNodeColor(p + p.RIGHT) == Color::DarkGray) 
+      visitables.push_back(p.RIGHT);
+
+  if (w->GetNodeColor(p + p.DOWN) == Color::DarkGray) 
+      visitables.push_back(p.DOWN);
+
+  if (w->GetNodeColor(p + p.LEFT) == Color::DarkGray) 
+      visitables.push_back(p.LEFT);
 
   return visitables;
 }
